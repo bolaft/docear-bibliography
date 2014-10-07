3cfd5ac8f347033a721152879dc1830e5aeb690a
bolaft <soufi@nsal.im>
2014-10-07 14:49:12 +0200

    initial commit

---
+++ _data/148E9EF00F77QK3BERU35KBEQMGEJQKMNC6Y/default_files/Thesis.mm
@@ -0,0 +1,6 @@
+<map version="freeplane 1.3.0" project="148E9EF00F77QK3BERU35KBEQMGEJQKMNC6Y" project_last_home="file:/home/soufian/workspace/docear-bibliography/">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Thesis" FOLDED="false" ID="ID_985300477" CREATED="1412675165475" MODIFIED="1412675165504">
+<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
+</node>
+</map>
---
+++ maps/literature_and_annotations.mm
@@ -0,0 +1,12 @@
+<map version="docear 1.1" type="literature_annotations" dcr_id="1412674152244_8mb6h4ep6ix7e7hj4l50wfzq3" project="148E9EF00F77QK3BERU35KBEQMGEJQKMNC6Y" project_last_home="file:/home/soufian/workspace/docear-bibliography/">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Literature &amp; Annotations" FOLDED="false" ID="ID_1276835448" CREATED="1412674152219" MODIFIED="1412674152244" DCR_PRIVACY_LEVEL="DEMO">
+<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
+<attribute NAME="mon_incoming_folder" VALUE="@@literature_repository@@"/>
+<attribute NAME="mon_mindmap_folder" VALUE="@@library_mindmaps@@"/>
+<attribute NAME="mon_auto" VALUE="2" OBJECT="java.lang.Integer|2"/>
+<attribute NAME="mon_subdirs" VALUE="2" OBJECT="java.lang.Integer|2"/>
+<attribute NAME="mon_add_extra_incoming_node" VALUE="2" OBJECT="java.lang.Integer|2"/>
+<attribute NAME="mon_flatten_dirs" VALUE="2" OBJECT="java.lang.Integer|2"/>
+</node>
+</map>
